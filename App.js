daa0ff0a6a9de3ca59a0742c97607eaf910c65fc
metelicaa <gogle3057230@mail.ru>
2017-12-22 18:24:53 +0300

    metelitca
    add roter

--- App.js
+++ App.js
@@ -1,40 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, UINavigationController } from 'react-native';
-import { BrowserRouter as Router, Link } from 'react-router-native';
+import { TabNavigator } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Header from './src/component/Header';
 
-const link = [
-  'test',
-  'test2',
-  'test4',
-  't32',
-];
-
-export default class App extends React.Component {
-  state = {
-    complex: null
-  };
-
-  componentDidMount() {
-    fetch('https://api.pik.ru/v1/block?types=1,2&metadata=1&statistics=1&images=1&locations=2,3')
-      .then(res => res.json())
-      .then(complex => this.setState({ complex }))
+const App = TabNavigator({
+  Home: {
+    screen: Header,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Ionicons
+          name={focused ? 'ios-home' : 'ios-home-outline'}
+          size={26}
+          style={{ color: tintColor }}
+        />
+      ),
+    },
   }
-
-  render() {
-    const { complex } = this.state;
-    return (
-      <Router style={styles.container}>
-        { link.map((item) => <Link to={ item }>{ item }</Link>) }
-      </Router>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
+
+export default App;
+
+// const link = [
+//   'test',
+//   'test2',
+//   'test4',
+//   't32',
+// ];
+//
+// export default class App extends React.Component {
+//   state = {
+//     complex: null
+//   };
+//
+//   componentDidMount() {
+//     fetch('https://api.pik.ru/v1/block?types=1,2&metadata=1&statistics=1&images=1&locations=2,3')
+//       .then(res => res.json())
+//       .then(complex => this.setState({ complex }))
+//   }
+//
+//   render() {
+//     const { complex } = this.state;
+//     return (
+//       <Router>
+//         <Stack key="root">
+//           <Scene key="login" component={Header} title="Header"/>
+//         </Stack>
+//       </Router>
+//     );
+//   }
+// }
+
